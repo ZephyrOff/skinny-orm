0787f43574f34f590460f96fd2402ad482c00a36
Alexandre Pajak <55457378+ZephyrOff@users.noreply.github.com>
2024-05-16 10:36:05 +0200

    Update orm.py

--- skinny_orm/orm.py
+++ skinny_orm/orm.py
@@ -6,20 +6,10 @@ class Orm:
     def __new__(self, connection, create_tables_if_not_exists=True, parse_fields=True, connection_type="sqlite3"):
         if isinstance(connection, str):
             if connection_type=="sqlite3":
-                self.connection = sqlite3.connect(connection)
-        else:
-            self.connection = connection
+                connection = sqlite3.connect(connection)
 
 
-        if 'sqlite3' in str(self.connection.__class__):
-            return SqliteOrm(self.connection, create_tables_if_not_exists, parse_fields)
+        if 'sqlite3' in str(connection.__class__):
+            return SqliteOrm(connection, create_tables_if_not_exists, parse_fields)
         else:
             raise NotImplementedError
-
-
-    def __enter__(self):
-        return self
-
-
-    def __exit__(self, *args):
-        self.connection.close()
